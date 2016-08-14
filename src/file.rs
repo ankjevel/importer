407b6c842106c165a860199acedd0cfa96d9760a
Dennis Pettersson <mail@dennispettersson.se>
2016-08-14 13:55:47 +0200

    Handle file-path

--- src/file.rs
+++ src/file.rs
@@ -12,12 +12,15 @@ use std::string::String;
 use std::path::{Path};
 use std::env::{current_dir as current, home_dir as home};
 
+const SLASH: char = '/' as char;
+const TIDLE: char = '~' as char;
+
 fn current_directory() -> String {
-    current().unwrap().into_os_string().into_string().unwrap()
+    current().unwrap().into_os_string().into_string().unwrap() + "/"
 }
 
 fn home_directory() -> String {
-    home().unwrap().into_os_string().into_string().unwrap()
+    home().unwrap().into_os_string().into_string().unwrap() + "/"
 } 
 
 fn string_to_static_str(s: String) -> &'static str {
@@ -29,25 +32,30 @@ fn string_to_static_str(s: String) -> &'static str {
 }
 
 pub fn unwrap_path<'a>(file_path: &&str) -> &'a Path {
-    const SLASH: char = '/' as char;
-    const TIDLE: char = '~' as char;
-
     let home_dir = &*home_directory();
     let current_dir = &*current_directory();
 
-    let mut owned_string = String::new();
-    
+    let mut file_path_copy = String::new();
+    file_path_copy.push_str(file_path);
+
+    let mut mutable_str = String::new();
     match file_path.as_bytes()[0] as char {
       SLASH => (),
-      TIDLE => owned_string.push_str(home_dir),
-      _ => owned_string.push_str(current_dir)
+      TIDLE => {
+          mutable_str.push_str(home_dir);
+          file_path_copy.remove(0);
+          match file_path_copy.chars().nth(0).unwrap() {
+              SLASH => { file_path_copy.remove(0); },
+              _ => ()
+          }
+      },
+      _ => {
+          mutable_str.push_str(current_dir);
+      }
     }
-   
-
-    let s: &'static str = string_to_static_str(owned_string);
-    let path = Path::new(s);
-
-    path
+    mutable_str.push_str(&file_path_copy);
+    let s: &'static str = string_to_static_str(mutable_str);
+    Path::new(s)
 }
 
 fn generate_md5(path: &Path) -> String {
