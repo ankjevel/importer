8721d9d6f8cbb9de250276f58f4b1a3b3d424ce4
Dennis Pettersson <mail@dennispettersson.se>
2016-08-15 15:22:10 +0200

    Read path from env

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,18 @@ mod file;
 mod config;
 mod string;
 
+use std::env::var;
+
+fn get_config_path() -> &'static str {
+    let default_value: &'static str = "~/Config.toml"; 
+    match var("CONFIG") {
+        Ok(val) => string::string_to_static_str(val),
+        Err(_) => default_value,
+    }
+}
+
 fn main() {
-    let mut conf = config::Config::new("~/Config.toml");
+    let mut conf = config::Config::new(get_config_path());
     let path = &conf.query("paths", "images");
 
     let mut files = file::Files::new();
